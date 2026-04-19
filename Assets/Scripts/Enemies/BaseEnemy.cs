25cef5c1f108e9af53ca081dc6626170183f3c2b
Jess <jw1519@student.aru.ac.uk>
2026-04-19 16:19:36 +0100

    added base enemy

--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -1,16 +1,33 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseEnemy : MonoBehaviour
+public class BaseEnemy : MonoBehaviour, ITakeDamage
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public string enemyName;
+    public int maxHealth;
+    public int currentHealth;
+    public int damage;
+    public List<BaseItem> lootTable;
+    public GameObject lootPrefab;
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    public void Die()
+    {
+        // Handle enemy death (e.g., play animation, drop loot, etc.)
+        if (lootPrefab != null && lootTable.Count > 0)
+        {
+            BaseItem lootItem = lootTable[Random.Range(0, lootTable.Count)];
+            GameObject loot = Instantiate(lootPrefab, transform.position, Quaternion.identity);
+            loot.GetComponent<ItemDrop>().SetItem(lootItem);
+        }
+        Destroy(gameObject);
     }
 }
